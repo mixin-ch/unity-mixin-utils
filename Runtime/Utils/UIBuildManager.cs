657deda3e3d413d35811e828a03e5e3c1da7fbd0
Adrian Schneider <adrian.schneider@rgbi.ch>
2022-12-31 14:46:59 +0100

    Update UIBuildManager.cs

--- Runtime/Utils/UIBuildManager.cs
+++ Runtime/Utils/UIBuildManager.cs
@@ -2,7 +2,7 @@ using UnityEngine.UIElements;
 
 namespace Mixin.Utils
 {
-    public abstract class UIBuildManager<T> : Singleton<T> where T : Singleton<T>
+    public abstract class UIBuildManager<T> : Singleton<T> where T : UIBuildManager<T>
     {
         protected VisualElement _root;
 
